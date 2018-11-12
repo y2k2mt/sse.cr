9ede49c398d47cd42803935e05e906126ab7c134
y2k2mt <y2.k2mt@gmail.com>
2018-11-12 17:47:32 +0900

    Refectoring

--- src/sse.cr
+++ src/sse.cr
@@ -2,7 +2,9 @@ require "uri"
 require "http/client"
 
 module HTTP::ServerSentEvents
+
   class EventSource
+
     record EventMessage, event : String, datas : Array(String), id : String, retry : Int64
 
     def initialize(host : String, path : String, port : Int32, tls = false, headers : HTTP::Headers = HTTP::Headers.new)
@@ -14,7 +16,7 @@ module HTTP::ServerSentEvents
       initialize(URI.parse(uri), headers)
     end
 
-    def initialize(@uri : URI, @base_headers : HTTP::Headers = HTTP::Headers.new)
+    def initialize(@uri : URI | Nil = nil, @base_headers : HTTP::Headers = HTTP::Headers.new)
       @abort = false
     end
 
@@ -27,29 +29,20 @@ module HTTP::ServerSentEvents
     def on_close(&@on_close : String ->)
     end
 
-    def run
-      open(@uri)
-    end
-
     def abort
       @abort = true
     end
 
-    protected def open(uri : URI, last_id : (String | Nil) = nil)
+    def run(uri : URI = @uri, last_id : (String | Nil) = nil)
+      if !uri
+        raise URI::Error.new("Endpoint URI must be specified")
+      end
       loop do
         if @abort
           break
         end
-        headers = HTTP::Headers.new
-        if last_id
-          headers.add("Last-Event-Id", "#{last_id}")
-        end
-        headers.add("Accept", "text/event-stream")
-        headers.add("Cache-Control", "no-cache")
-        headers.merge! @base_headers
-        HTTP::Client.get(uri, headers: headers) do |response|
-          status_code = response.status_code
-          case status_code
+        HTTP::Client.get(uri, headers: prepare_headers(last_id)) do |response|
+          case response.status_code
           when 200
             retry = 0.to_i64
             an_entry = [] of String
@@ -90,9 +83,9 @@ module HTTP::ServerSentEvents
           when 503
             body = response.body_io?
             if body
-              @on_error.try &.call({status_code: status_code, message: body.gets_to_end})
+              @on_error.try &.call({status_code: response.status_code, message: body.gets_to_end})
             else
-              @on_error.try &.call({status_code: status_code, message: ""})
+              @on_error.try &.call({status_code: response.status_code, message: ""})
             end
             # Ignore date formatted header
             retry_after = response.headers["Retry-After"].to_i64?
@@ -104,14 +97,24 @@ module HTTP::ServerSentEvents
           else
             body = response.body_io?
             if body
-              @on_error.try &.call({status_code: status_code, message: body.gets_to_end})
+                @on_error.try &.call({status_code: response.status_code, message: body.gets_to_end})
             else
-              @on_error.try &.call({status_code: status_code, message: ""})
+                @on_error.try &.call({status_code: response.status_code, message: ""})
             end
             @abort = true
           end
         end
       end
+    end
+
+    private def prepare_headers(last_id : String | Nil)
+      headers = HTTP::Headers.new
+      if last_id
+        headers.add("Last-Event-Id", "#{last_id}")
+      end
+      headers.add("Accept", "text/event-stream")
+      headers.add("Cache-Control", "no-cache")
+      headers.merge! @base_headers
     end
 
     private def parse_to_hash(entry : Array(String)) : Hash(String, (String | Array(String)))
@@ -131,5 +134,7 @@ module HTTP::ServerSentEvents
       }
       event_source
     end
+
   end
+
 end
