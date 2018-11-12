06911588ef7d66849e8d02d77951309750d71b8a
y2k2mt <y2.k2mt@gmail.com>
2018-11-12 12:40:52 +0900

    Resolve stack overflow when using long polling

--- src/sse.cr
+++ src/sse.cr
@@ -35,75 +35,80 @@ module HTTP::ServerSentEvents
       @abort = true
     end
 
-    protected def open(uri : URI, last_event_id : String | Nil = nil)
-      headers = HTTP::Headers.new
-      if last_event_id
-        headers.add("Last-Event-Id", "#{last_event_id}")
-      end
-      headers.add("Accept", "text/event-stream")
-      headers.add("Cache-Control", "no-cache")
-      headers.merge! @base_headers
-      HTTP::Client.get(uri, headers: headers) do |response|
-        status_code = response.status_code
-        case status_code
-        when 200
-          last_id = ""
-          retry = 0.to_i64
-          an_entry = [] of String
-          io = response.try &.body_io
-          loop do
-            if @abort
-              break
-            end
-            line = io.gets
-            if line
-              if line.empty? && an_entry.size != 0
-                entry_hash = parse_to_hash(an_entry)
-                last_id = entry_hash["id"].as(String)
-                retry? = entry_hash["retry"].as(String).to_i64?
-                if retry?
-                  retry = retry?
-                end
-                @on_message.try &.call(EventMessage.new(
-                  id: last_id,
-                  datas: entry_hash["datas"].as(Array(String)),
-                  retry: retry,
-                  event: entry_hash["event"].as(String)
-                ))
-                an_entry = [] of String
-              else
-                an_entry = an_entry << line
+    protected def open(uri : URI, last_id : (String | Nil) = nil)
+      loop do
+        if @abort
+          break
+        end
+        headers = HTTP::Headers.new
+        if last_id
+          headers.add("Last-Event-Id", "#{last_id}")
+        end
+        headers.add("Accept", "text/event-stream")
+        headers.add("Cache-Control", "no-cache")
+        headers.merge! @base_headers
+        HTTP::Client.get(uri, headers: headers) do |response|
+          status_code = response.status_code
+          case status_code
+          when 200
+            retry = 0.to_i64
+            an_entry = [] of String
+            io = response.try &.body_io
+            loop do
+              if @abort
+                break
               end
-            else
-              break
+              line = io.gets
+              if line
+                if line.empty? && an_entry.size != 0
+                  entry_hash = parse_to_hash(an_entry)
+                  last_id = entry_hash["id"].as(String)
+                  retry? = entry_hash["retry"].as(String).to_i64?
+                  if retry?
+                    retry = retry?
+                  end
+                  @on_message.try &.call(EventMessage.new(
+                    id: last_id,
+                    datas: entry_hash["datas"].as(Array(String)),
+                    retry: retry,
+                    event: entry_hash["event"].as(String)
+                  ))
+                  an_entry = [] of String
+                else
+                  an_entry = an_entry << line
+                end
+              else
+                break
+              end
             end
-          end
-          if !last_id.empty? && !@abort
-            sleep retry / 1000
-            open(uri, last_id)
-          end
-        when 307
-          location = response.headers["Location"]
-          open(URI.parse(location))
-        when 503
-          body = response.body_io?
-          if body
-            @on_error.try &.call({status_code: status_code, message: body.gets_to_end})
+            if (last_id && !last_id.empty?) && !@abort
+              sleep retry / 1000
+            end
+          when 307
+            location = response.headers["Location"]
+            uri = URI.parse(location)
+          when 503
+            body = response.body_io?
+            if body
+              @on_error.try &.call({status_code: status_code, message: body.gets_to_end})
+            else
+              @on_error.try &.call({status_code: status_code, message: ""})
+            end
+            # Ignore date formatted header
+            retry_after = response.headers["Retry-After"].to_i64?
+            if retry_after
+              sleep retry_after / 1000
+            else
+              @aboart = true
+            end
           else
-            @on_error.try &.call({status_code: status_code, message: ""})
-          end
-          # Ignore date formatted header
-          retry_after = response.headers["Retry-After"].to_i64?
-          if retry_after
-            sleep retry_after / 1000
-            open(uri)
-          end
-        else
-          body = response.body_io?
-          if body
-            @on_error.try &.call({status_code: status_code, message: body.gets_to_end})
-          else
-            @on_error.try &.call({status_code: status_code, message: ""})
+            body = response.body_io?
+            if body
+              @on_error.try &.call({status_code: status_code, message: body.gets_to_end})
+            else
+              @on_error.try &.call({status_code: status_code, message: ""})
+            end
+            @abort = true
           end
         end
       end
