8933da680ce148f7a0f5daa5833fd3a255bcece2
y2k2mt <y2.k2mt@gmail.com>
2020-08-22 21:54:58 +0900

    Log appeared in stub server

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -6,6 +6,8 @@ require "log"
 def start_stub_server
   port = Random.rand(40000..65535)
   spawn do
+    backend = Log::IOBackend.new
+    Log.builder.bind "http.server_sent_events", :debug, backend
     address = HTTP::ServerSentEvents.current_stub_server.bind_tcp port
     puts "Listening on http://#{address}"
     HTTP::ServerSentEvents.current_stub_server.listen
@@ -16,8 +18,5 @@ end
 def stop_stub_server
   HTTP::ServerSentEvents.stop_current_stub_server
 end
-
-backend = Log::IOBackend.new
-Log.builder.bind "http.server_sent_events", :debug, backend
 
 SPEC_SERVER_PORT = start_stub_server
